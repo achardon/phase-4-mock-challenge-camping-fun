d815399c34bf652b2e7d04ff2bb971cb89a91258
Alessandra Chardon <achardon@ucla.edu>
2022-02-14 11:00:00 -0500

    all tests passing

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -2,10 +2,10 @@ class SignupsController < ApplicationController
 
     def create
         signup = Signup.create(signup_params)
-        if signup
+        if signup.valid?
           render json: signup.activity, status: :created
         else
-          render json: {errors: "validation errors"}, status: :unprocessable_entity
+          render json: {errors: signup.errors.full_messages}, status: :unprocessable_entity
         end
       end
     
